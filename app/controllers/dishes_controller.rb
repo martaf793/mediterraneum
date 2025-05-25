7edcba3997ded0744bb4aeaa783814fae59c9b8c
martaf793 <martaf@uchicago.edu>
2025-05-25 21:48:09 +0000

    desplegable

--- app/controllers/dishes_controller.rb
+++ app/controllers/dishes_controller.rb
@@ -1,6 +1,6 @@
 class DishesController < ApplicationController
   def index
-    matching_dishes = Dish.all
+    @matching_dishes = Dish.all
 
     @list_of_dishes = matching_dishes.order({ :created_at => :desc })
 
